49b8e6d513054c0d8529aac4c2f9578b3df2bda2
Marco G. Maia <marcogmaia@gmail.com>
2017-06-04 20:12:36 -0300

    partial q1 server/client

--- q1_client.py
+++ q1_client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import socket
+import sys
 
 host = 'localhost'
 port = 5555
@@ -34,13 +35,20 @@ class protocol():
             self.sock.connect((host, port))
             while 1:
                 data = input()
-                self.sock.send(bytes(data, 'utf8'))
+                if data == '\x18': # CTRL+x pra terminar
+                    self.sock.send(bytes('stop', 'utf8'))
+                    sys.exit()
+                else:
+                    self.sock.send(bytes(data, 'utf8'))
 
         elif self.protocol == 'udp':
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             while 1:
                 data = input()
-                self.sock.sendto(bytes(data, 'utf8'), (host, port))
+                if data == '\x18':
+                    self.sock.sendto(bytes('stop', 'utf8'), (host, port))
+                else:
+                    self.sock.sendto(bytes(data, 'utf8'), (host, port))
 
         else:
             print('ERROR: protocol not defined')
